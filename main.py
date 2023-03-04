a8c7ceb9047791e276c16cb49583467528546c25
eusoulucas_personal <lucadito@gmail.com>
2023-03-04 12:17:10 -0300

    ediçoes finais

--- main.py
+++ main.py
@@ -1,100 +1,92 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from tqdm import tqdm
+import time
 
-def listar_seguidores(n):
-    accounts = []
+def scraping(categorie):
+    prev_count = 0
+    curr_count = 0
 
-    account = str(username)
-    print('Followers of the "{}" account'.format(account))
-    follower_css = "div._ab9-:nth-child({})"
-    for group in tqdm(range(1, n, 12)):  # Scrape 100 followers in groups of 10
-        for follower_index in range(group, group + 12):
-            try:
-                # Scroll to the last follower in the group
-                last_follower = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, follower_css.format(follower_index))))
-                
-                driver.execute_script("arguments[0].scrollIntoView();", last_follower)
-                accounts.append(last_follower.text)
+    # Wait for the profile page to load
+    driver.get(url + USERNAME + '/'+ categorie +'/')
 
-            except Exception as e:
-                print(e)
-                accounts.append(last_follower.text)
-    return accounts
-
-
-def get_first_element(string):
-    return string.split("\n")[0]
-            
-
-followers_list = []
-following_list = []
-
-# Prompt the user to enter their Instagram username and password
-username = input("Enter your Instagram username: ")
-password = input("Enter your Instagram password: ")
-
-# Set up the Firefox webdriver and navigate to Instagram login page
-driver = webdriver.Firefox()
-driver.get("https://www.instagram.com/accounts/login/")
-time.sleep(10)
-
-'''
-try:
-    #Closing popup
-    cookies_bt = driver.find_elements(By.XPATH, "//button[text()='Permitir apenas cookies essenciais']")
-    #print(cookies_bt)
-    cookies_bt[0].click()
+    # get the users list element
     time.sleep(5)
-except Exception as e:
-    print(e)
-'''
+    users_list = driver.find_element(By.CLASS_NAME, '_aano')
 
-# Enter the user's Instagram username and password
-username_field = driver.find_element(By.CSS_SELECTOR, "input[name='username']")
-password_field = driver.find_element(By.CSS_SELECTOR, "input[name='password']")
-username_field.send_keys(username)
-password_field.send_keys(password)
-password_field.send_keys(Keys.ENTER)
-time.sleep(10)
+    # get the initial number of followers in the list
+    prev_count = len(users_list.find_elements(By.CLASS_NAME, 'xt0psk2'))
 
-# Navigate to the user's profile and get their followers
-driver.get(f"https://www.instagram.com/{username}")
-time.sleep(10)
+    # scroll to the bottom of the list until no more followers are loaded
+    while True:
+        try:
+            # scroll to the bottom of the list
+            driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', users_list)
+            time.sleep(5) # wait for the next set of followers to load
 
-count = driver.find_elements(By.CLASS_NAME, '_ac2a')
-n_follower = int(count[1].text.replace('.',''))
-n_following = int(count[2].text.replace('.',''))
-time.sleep(5)
+            # get the new number of followers in the list
+            curr_count = len(users_list.find_elements(By.CLASS_NAME, 'xt0psk2'))
+            
+            if curr_count <= prev_count:
+                break
+            prev_count = curr_count
 
-follows = driver.find_elements(By.XPATH, '//a[@href="/{}/followers/"]'.format(username))
-print(follows[0])
-follows[0].click()
-time.sleep(5)
+        except Exception as e:
+            print(e)
+            break
 
-followers_list = listar_seguidores(n_follower)
+    users = users_list.find_elements(By.CLASS_NAME, 'xt0psk2')
 
-# Navigate to the user's following and get the list of users they're following
-driver.get(f"https://www.instagram.com/{username}/following/")
-time.sleep(2)
+    current_users = []
+    for user in users:
+        current_users.append(user.text)
+    
+    lista_sem_duplicatas = list(set(current_users))
 
-following_list = listar_seguidores(n_following)
+    return lista_sem_duplicatas
 
-followers_list_clean = [get_first_element(s) for s in followers_list]
-following_list_clean = [get_first_element(s) for s in following_list]
+# Replace with your Instagram credentials
+USERNAME = str(input('Digite o usuário:'))
+PASSWORD = str(input("Digite a senha:"))
 
-# Identify the users the user is following that are not following them back
-not_following_back = [user for user in following_list_clean if user not in followers_list_clean]
+# Initialize the Chrome browser and navigate to Instagram
+driver = webdriver.Chrome()
+url = 'https://www.instagram.com/'
+driver.get(url)
 
-# Print the list of users the user is following that are not following them back
-print("Users you're following that are not following you back:")
-for people in not_following_back:
-    print(people)
+# Creating a waiter
+wait = WebDriverWait(driver, 10)
 
-# Close the Firefox webdriver
+# Log in to Instagram
+username_field = wait.until(EC.presence_of_element_located((By.NAME, 'username')))
+password_field = wait.until(EC.presence_of_element_located((By.NAME, 'password')))
+username_field.send_keys(USERNAME)
+password_field.send_keys(PASSWORD)
+login_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[3]/button')))
+login_button.click()
+
+for i in range(2):
+    try:
+        # Wait for the login process to complete
+        not_now_button = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Not Now"]')))
+        not_now_button.click()
+
+    except Exception as e:
+        # Wait for the login process to complete
+        not_now_button = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Agora não"]')))
+        not_now_button.click()
+
+# Navigate to your profile page
+profile_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@href="/' + USERNAME + '/"]')))
+profile_button.click()
+
+following = scraping('following')
+followers = scraping('followers')
+
+for username in following:
+    if username not in followers:
+        print(username + " is not following you back.")
+
+# Quit the browser
 driver.quit()
